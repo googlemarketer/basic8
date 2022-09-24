083cfcda6632bca3d41aad6e42f935fb7da13a89
googlemarketer <trustrajeshg@gmail.com>
2022-09-24 12:56:15 +0530

    finalcommit on 240222

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,5 +14,6 @@ const mix = require('laravel-mix');
 
 
 mix.js('resources/js/main.js', 'public/js')
-    .vue()
-    .sass('resources/sass/app.scss', 'public/css');
+    .vue(3)
+    .sass('resources/sass/app.scss', 'public/css')
+    .version();
